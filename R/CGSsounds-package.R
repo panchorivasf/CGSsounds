477e0eed525d0709aae189f4436398d283f98e70
naturewaves <rthoptera@gmail.com>
2025-07-14 15:33:20 -0400

    add main help file

---
+++ R/CGSsounds-package.R
@@ -0,0 +1,18 @@
+#' CGSsounds: A package for accessing soundscape samples
+#'
+#' The CGSsounds package provides examples of wave objects extracted
+#' from the collection of sounds in the Center for Global Soundscapes,
+#' Purdue University, West Lafayette IN, United States.
+#'
+#'
+#' @docType package
+#' @name CGSsounds
+#' @aliases CGSsounds-package
+#'
+#' @import tuneR
+#' @import seewave
+#'
+#' @references
+#' For more information see:
+#' \url{https://github.com/panchorivasf/CGSsounds}
+NULL
